f78569897903d28bb1aedb812fef55a0352f8e19
Sou-o-Renan <renanalexandre18@hotmail.com>
2024-02-17 10:14:37 -0300

    CÓDIGO ATUALIZADO
    ADICIONADO MOSTRAR PRODUTOS POR LETRA, CÓDIGO EXPLICADO

--- 17-02-24/Produto.cs
+++ 17-02-24/Produto.cs
@@ -1,15 +1,22 @@
 namespace DesafioProduto;
 
 public class Produto
-{
+{         // TUDO QUE PRECISA NO PRODUTO
     public int ID { get; set; }
     public string Nome { get; set; }
     public DateOnly DataValidade { get; set; }
-    public List <Produto> Produtos { get; set; } // LISTA DE PRODUTOS
-    = new List<Produto>();
 
-// MÉTODO DE CADASTRAR O PRODUTO
-    public void CadastrarProduto(string nome, DateOnly validade)
+//____________________________________________________________________//
+//________________________LISTA DE PRODUTOS________________________________//
+
+    public List <Produto> Produtos { get; set; } 
+    = new List<Produto>();
+//____________________________________________________________________//
+
+
+//______________________MÉTODO DE CADASTRAR O PRODUTO________________________________//
+
+    public void CadastrarProduto(string nome, DateOnly validade) // método de cadastrar um produto
     {
         Produto produto = new Produto()
         { Nome = nome, DataValidade = validade};
@@ -17,8 +24,27 @@ public class Produto
 
     Produtos.Add(produto);
     }
+//____________________________________________________________________//
 
-    public void MostrarProdutos()
+
+//____________________________________________________________________//
+
+
+// MÉTODO PARA MOSTRAR PRODUTOS POR FILTRO DA LETRA
+    public void MostrarProdutosLetra(char LetraFiltro) // método de filtrar por letra inicial um produto
+    {
+
+        foreach(var produto in Produtos){
+            if(produto.Nome[0].ToString().ToUpper() == LetraFiltro.ToString().ToUpper()){
+             Console.WriteLine($"Produto: {produto.Nome}  Validade: {produto.DataValidade}  ID: {produto.ID}");
+
+            }
+        }
+
+
+    }
+
+    public void MostrarProdutos() // método de mostrar todos os produtos
     {
         foreach(var prod in Produtos){
             Console.WriteLine($"Produto: {prod.Nome}  Validade: {prod.DataValidade}  ID: {prod.ID}");
--- 17-02-24/Program.cs
+++ 17-02-24/Program.cs
@@ -16,5 +16,13 @@ using DesafioProduto;
 
 
 Produto produto = new Produto();
-produto.CadastrarProduto("Leite", new DateOnly(2024,01,03));
-produto.MostrarProdutos();
+produto.CadastrarProduto("Leite", new DateOnly(2024,01,03)); // copie apenas essa linha para criar um novo produto
+produto.CadastrarProduto("Arroz", new DateOnly(2024,09,05));
+produto.CadastrarProduto("café", new DateOnly(2023,10,02));
+produto.CadastrarProduto("Carne", new DateOnly(2024,02,17));
+produto.CadastrarProduto("Chocolate", new DateOnly(2024,02,19));
+
+
+
+produto.MostrarProdutosLetra('C'); // usando o método de filtro de letra inicial
+produto.MostrarProdutos(); // usando o método de mostrar todos os produtos
